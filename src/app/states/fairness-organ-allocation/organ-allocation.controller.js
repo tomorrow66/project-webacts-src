cdcb60de5a18872b58ab3fa894cb460f3ab9f911
DeAndre Johnson <dre@phreshr.com>
2015-05-11 16:57:56 -0400

    project changes

--- src/app/states/fairness-organ-allocation/organ-allocation.controller.js
+++ src/app/states/fairness-organ-allocation/organ-allocation.controller.js
@@ -6,7 +6,7 @@
 function organAllocationCtrl ($rootScope, $sce, $state, $log) {
   /* jshint validthis:true */
   var vm = this;
-
+  vm.title = $state.current.title;
   activate();
 
   function activate() {
